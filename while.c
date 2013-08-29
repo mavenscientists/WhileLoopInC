b915c12a32a6ece13e55293ed36de242eab90638
Karan <karan4c6@gmail.com>
2013-08-29 18:16:50 +0530

    while loop

---
+++ while.c
@@ -0,0 +1,46 @@
+  /**  
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * Copyright 2013 Maven Scientists http://mavenscientists.com
+    */
+
+ /**
+   * @author	Maven Scientists (http://mavenscientists.com) 
+   * @email	info@mavenscientists.com
+   */
+
+#include <stdio.h>
+
+int main()
+{
+	int I = 10;
+	
+	while( I >= 1)
+	{
+		printf("%d\n", I);
+		I -= 2;
+	}
+/*
+While Loop Syntax
+
+initialization
+while( condition)
+{
+	statement 1;
+	statement 2;
+
+	increment or decrement;
+}
+statement3;
+
+*/	
+	return 0;
+}
